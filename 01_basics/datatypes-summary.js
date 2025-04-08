0baae9722b331fcf5c7120967832ddff74a4c4cf
Gunjan Jain <80124279+gunjan02jain@users.noreply.github.com>
2025-04-08 22:53:35 +0000

    datatype

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,32 @@
+// PRIMITIVE
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// const bigNumber = 6545545445454n
+
+// REFERENCE TYPE (Non primitive)
+// Array, Objects, Functions
+
+const heros = ['shaktiman', 'nagraj', 'doga'];
+
+let myObj =
+{
+    name: 'Gunjan',
+    age: 22,
+}
+
+const myFunction = function(){
+ console.log('Hello world');
+ 
+}
+
+console.log(typeof bigNumber);

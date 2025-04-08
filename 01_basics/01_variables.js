9e295eaccef5d3cb2304319f6dc7dddc50674f36
Gunjan Jain <80124279+gunjan02jain@users.noreply.github.com>
2025-04-08 06:32:00 +0000

    compa

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,9 @@
+const accountId = 144553
+let accountEmail = 'gunjan@email.com'
 
+// 
+
+accountEmail = 'myemail@email.com'
+
+console.log(accountId);
+console.table([accountId, accountEmail])
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = '33'
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// '33' => 33 number
+// "33abc" => NaN
+//  ture =>1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Gunjan" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************************** Operations ****************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
